131e80bf7796bd8cc19716dccff7e096c178be82
Samuel Mimram <smimram@gmail.com>
2020-06-12 09:35:44 +0200

    Progressing.

--- src/lang.ml
+++ src/lang.ml
@@ -1,64 +1,75 @@
 (** Definition of a generator. *)
 type gen =
   {
-    gen_opts : (string * string option) list;
-    gen_typ : int * int;
+    gen_opts : (string * string) list; (** list of optional parameters and their value (a=b) *)
+    gen_typ : int * int; (** type: number of inputs and outputs *)
   }
 
 (** Expression for a cell. *)
 type expr =
-  | Ident of string
-  | Id of int
-  | Comp of int option * expr * expr
+  | Gen of string (** a generator *)
+  | Id of int (** identity *)
+  | Comp of int option * expr * expr (** composite, the number is the dimension in which they are composed (max-1 if not specified) *)
 
 (** Declarations. *)
 type t =
   {
-    gens : (string * gen) list;
-    cells : (int * expr) list
+    gens : (string * gen) list; (** gneerators *)
+    cells : (int * expr) list (** cells (which are numbered in order to be able to identify them in LaTeX) *)
   }
 
+(** Empty declaration list. *)
 let decls_empty = { gens = []; cells = [] }
 
-let add_gen l d =
-  { l with gens = d::l.gens }
+(** Add a generator. *)
+let add_gen l (name,gen) =
+  { l with gens = (name,gen)::l.gens }
 
+(** Dimension of a cell. *)
 let rec dim = function
-  | Ident _ -> 2
+  | Gen _ -> 2
   | Id _ -> 1
   | Comp (_, e1, e2) -> max (dim e1) (dim e2)
 
+(** Typing error. *)
 exception Typing of string
 
+(** Type of a cell. *)
 let rec typ gens = function
-  | Ident s ->
-     (
-       try
-         (List.assoc s gens).gen_typ
-       with
-       | Not_found -> raise (Typing ("unknown cell " ^ s))
-     )
+  | Gen s ->
+    (
+      try
+        (List.assoc s gens).gen_typ
+      with
+      | Not_found -> raise (Typing ("unknown cell " ^ s))
+    )
   | Id n -> n, n
   | Comp (n, e1, e2) ->
-     let n =
-       match n with
-       | Some n -> n
-       | None -> min (dim e1) (dim e2) - 1
-     in
-     let s1, t1 = typ gens e1 in
-     let s2, t2 = typ gens e2 in
-     if n = 0 then (s1+s2, t1+t2)
-     else if n = 1 then
-       (
-         (
-           if t1 <> s2 then
+    let n =
+      match n with
+      | Some n -> n
+      | None -> min (dim e1) (dim e2) - 1
+    in
+    let s1, t1 = typ gens e1 in
+    let s2, t2 = typ gens e2 in
+    match n with
+    | 0 -> s1+s2, t1+t2
+    | 1 ->
+        (
+          if t1 <> s2 then
             let err = Printf.sprintf "cannot compose target %d with source %d" t1 s2 in
             raise (Typing err)
-         );
-         s1, t2
-       )
-     else assert false
+        );
+        s1, t2
+    | _ ->
+      (* n-dimensional diagrams with n>2 are not (yet) supported *)
+      assert false
 
+(** Add a cell to declarations. *)
 let add_cell l c =
   ignore (typ l.gens (snd c));
   { l with cells = c::l.cells }
+
+(* module Matrix = struct *)
+  (* type t =  *)
+(* end *)
--- src/parser.mly
+++ src/parser.mly
@@ -11,35 +11,37 @@
 
 %start decls
 %type <Lang.t> decls
+%left COMP
 %%
 
 decls:
-    | indecls EOF { $1 }
+  | indecls EOF { $1 }
 
 indecls:
-    | indecls gen { add_gen $1 $2 }
-    | indecls cell { add_cell $1 $2 }
-    | { decls_empty }
+  | indecls gen { add_gen $1 $2 }
+  | indecls cell { add_cell $1 $2 }
+  | { decls_empty }
 
 gen:
-    | GEN opts LACC STRING COLON INT TO INT RACC { $4, { gen_opts = $2; gen_typ = ($6, $8) } }
+  | GEN opts LACC STRING COLON INT TO INT RACC { $4, { gen_opts = $2; gen_typ = ($6, $8) } }
 
 opts:
-    | { [] }
-    | LBRA inopts RBRA { $2 }
+  | { [] }
+  | LBRA inopts RBRA { $2 }
 
 inopts:
-    | opt { [$1] }
-    | opt COMMA inopts { $1::$3 }
+  | opt { [$1] }
+  | opt COMMA inopts { $1::$3 }
 
 opt:
-    | STRING { $1,None }
+  | STRING { $1,"" }
+  | STRING EQ STRING { $1, $3 }
 
 cell:
-    | CELL LACC expr RACC { $1,$3 }
+  | CELL LACC expr RACC { $1,$3 }
 
 expr:
-    | STRING { Ident $1 }
-    | INT { Id $1 }
-    | expr COMP expr { Comp ($2,$1,$3) }
-    | LPAR expr RPAR { $2 }
+  | STRING { Gen $1 }
+  | INT { Id $1 }
+  | expr COMP expr { Comp ($2,$1,$3) }
+  | LPAR expr RPAR { $2 }
