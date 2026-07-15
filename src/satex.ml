29e87aaa13586fccb14f94af490c95eae2d874d4
Samuel Mimram <smimram@gmail.com>
2026-07-15 20:04:15 +0200

    Bump version.

--- src/satex.ml
+++ src/satex.ml
@@ -55,7 +55,7 @@ let () =
          [
            "--horizontal", Arg.Set Settings.horizontal, " Horizontal layout for composition.";
            "--tikzpicture-option", Arg.String (fun o -> Lang.global_options := o :: !Lang.global_options), " Option for tikzpicture.";
-           "--version", Arg.Unit (fun () -> print_endline "satex version 0.0"; exit 0), " Show version."
+           "--version", Arg.Unit (fun () -> print_endline "satex version 0.1.3"; exit 0), " Show version."
          ]
       ) (fun s -> fname := s) usage;
     if !fname = "" then Common.error "Please provide a .satex file name as input.";
